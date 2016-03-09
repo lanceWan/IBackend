485da7f7c77ceadde74984cfab2e2b4fc0854292
hsky <wen.zhou@bioon.com>
2016-03-09 17:39:53 +0800

    修改菜单表结构

--- database/migrations/2016_03_04_071547_create_menus_table.php
+++ database/migrations/2016_03_04_071547_create_menus_table.php
@@ -16,6 +16,10 @@ class CreateMenusTable extends Migration
             $table->increments('id');
             $table->integer('parent_id')->default(0)->comment('菜单关系');
             $table->string('name')->default('')->comment('菜单名称');
+            
+            $table->string('language')->default('')->comment('语言包');
+
+            $table->string('icon')->default('')->comment('图标');
             $table->string('slug')->default('')->comment('菜单对应的权限');
             $table->string('url')->default('')->comment('菜单链接地址');
             $table->string('description')->default('')->comment('描述');
