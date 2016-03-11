69885c0b2e5d8830b1043ebaef4c24ce3623caab
hsky <wen.zhou@bioon.com>
2016-03-11 15:20:21 +0800

    修改创建种子数据顺序

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
     	Model::unguard();
-        $this->call(UserTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(MenusTableSeeder::class);
         Model::reguard();
     }
